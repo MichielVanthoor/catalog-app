a760f4e15609210651227d59593dadb6f31ef383
Michiel Vanthoor <michielvanthoor@gmail.com>
2018-05-01 20:14:26 +0200

    Implementing ORM in database_setup.py file and populating categories

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,10 +1,23 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, Item
 app = Flask(__name__)
 
+# Create session and connect to database
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 def HelloWorld():
-    return "Hello World"
+    categories = session.query(Category).all()
+    output = ''
+    for i in categories:
+        output += i.name
+        output += '</br>'
+    return output
 
 if __name__ == '__main__':
     app.debug = True
---
+++ vagrant/catalog/database_setup.py
@@ -0,0 +1,37 @@
+# Configuration code for ORM
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Class definition
+class Category(Base):
+    # Table definition
+    __tablename__ = 'category'
+
+    # Mapper information
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+# Class definition
+class Item(Base):
+    # Table definition
+    __tablename__ = 'item'
+
+    # Mapper information
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(500))
+    timestamp = Column(String(250), nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+# Configuration code for ORM 
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
